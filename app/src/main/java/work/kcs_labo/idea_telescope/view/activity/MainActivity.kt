8aef4756be413969f8ecffb9fd436ba409523849
Hide-KC <kcs.dev.labo@gmail.com>
2020-09-07 19:05:24 +0900

    main_activity.xml convert to Data Binding layout file

---
+++ app/src/main/java/work/kcs_labo/idea_telescope/view/activity/MainActivity.kt
@@ -0,0 +1,15 @@
+package work.kcs_labo.idea_telescope.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import work.kcs_labo.idea_telescope.R
+import work.kcs_labo.idea_telescope.databinding.MainActivityBinding
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    val binding = DataBindingUtil.setContentView<MainActivityBinding>(this, R.layout.main_activity)
+
+  }
+}
